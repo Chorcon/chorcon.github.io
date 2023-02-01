b9802d66205ef03be06a0f4a056af793649fb89a
Chorcon <Chorcon@users.noreply.github.com>
2023-02-01 21:03:20 +0100

    First work on BookLook

--- _personal_training_projects/booklook/script.js
+++ _personal_training_projects/booklook/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Declare variables for manipulating DOM
+const searchInput = document.querySelector('#searchInput');
+const searchButton = document.querySelector('#searchButton');
+
+// Add event listeners
+searchInput.addEventListener('keypress', searchBook);
+searchButton.addEventListener('click', searchBook);
+
+function searchBook(e) {
+  // If a keypress is not enter, stop looking
+  if (e.type === 'keypress' && e.key !== 'Enter') return;
+  const searchString = searchInput.value; // fetch search string
+  if (!searchString) return; // abort if no string
+
+  console.log(searchString);
+}
